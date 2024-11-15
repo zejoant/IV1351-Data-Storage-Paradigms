f117ece1ba7062ceb01e4b4c8e6f3bdf7533dd8e
zejoant <77977238+zejoant@users.noreply.github.com>
2024-11-15 13:42:56 +0100

    done
    ball

--- Task 2/sem2 create database.sql
+++ Task 2/sem2 create database.sql
@@ -16,41 +16,48 @@ CREATE TABLE "inventory" (
 CREATE TABLE "instrument" (
   id SERIAL PRIMARY KEY,
   price INT,
-  type VARCHAR(500) NOT NULL REFERENCES inventory,
+  instrument_type VARCHAR(500) NOT NULL REFERENCES inventory ON DELETE CASCADE ON UPDATE CASCADE,
   is_rented BOOLEAN NOT NULL
 );
 
 CREATE TABLE "instructor" (
   id SERIAL PRIMARY KEY,
   can_teach_ensemble BOOLEAN NOT NULL,
-  person_id INT NOT NULL REFERENCES person,
-  taught_instruments VARCHAR(500) NOT NULL
+  person_id INT NOT NULL REFERENCES person ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE "instructor_taught_instrument" (
+  instructor_id INT NOT NULL REFERENCES instructor ON DELETE CASCADE ON UPDATE CASCADE,
+  instrument_type VARCHAR(500) NOT NULL REFERENCES inventory ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY (instructor_id, instrument_type)
 );
 
 CREATE TABLE "instructor_availability" (
-  instructor_id INT NOT NULL REFERENCES instructor,
-  date DATE,
-  time TIME,
-  duration INT
+  instructor_id INT NOT NULL REFERENCES instructor ON DELETE CASCADE ON UPDATE CASCADE,
+  time TIMESTAMP,
+  duration INT,
+  PRIMARY KEY (instructor_id, time)
 );
 
 CREATE TABLE "student" (
   id SERIAL PRIMARY KEY,
-  instrument_studied VARCHAR(500),
+  instrument_studied VARCHAR(500) NOT NULL REFERENCES inventory ON DELETE SET NULL ON UPDATE CASCADE,
   sibling_id INT,
   monthly_fee INT,
   skill_level VARCHAR(500),
-  person_id INT NOT NULL REFERENCES person
+  person_id INT NOT NULL REFERENCES person ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE "sibling_discount" (
   amount FLOAT,
-  student_id INT NOT NULL REFERENCES student
+  student_id INT NOT NULL REFERENCES student ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY (student_id)
 );
 
 CREATE TABLE "rented_instrument" (
-  student_id INT NOT NULL REFERENCES student,
-  instrument_id INT NOT NULL REFERENCES instrument
+  student_id INT NOT NULL REFERENCES student ON DELETE CASCADE ON UPDATE CASCADE,
+  instrument_id INT NOT NULL REFERENCES instrument ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY (student_id, instrument_id)
 );
 
 CREATE TABLE "lesson" (
@@ -58,38 +65,44 @@ CREATE TABLE "lesson" (
   type VARCHAR(500),
   price INT,
   skill_level VARCHAR(500),
-  instructor_id INT NOT NULL REFERENCES instructor
+  instructor_id INT NOT NULL REFERENCES instructor ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 CREATE TABLE "student_lesson_list" (
-  student_id INT NOT NULL REFERENCES student,
-  lesson_id INT NOT NULL REFERENCES lesson
+  student_id INT NOT NULL REFERENCES student ON DELETE CASCADE ON UPDATE CASCADE,
+  lesson_id INT NOT NULL REFERENCES lesson ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY (student_id, lesson_id)
 );
 
 CREATE TABLE "lesson_time_slot" (
   date DATE,
   time TIME,
   duration INT,
-  lesson_id INT NOT NULL REFERENCES lesson
+  lesson_id INT NOT NULL REFERENCES lesson ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY (lesson_id)
 );
 
 CREATE TABLE "individual_lesson" (
-  instrument_type VARCHAR(500) NOT NULL REFERENCES inventory,
-  lesson_id INT NOT NULL REFERENCES lesson
+  instrument_type VARCHAR(500) NOT NULL REFERENCES inventory ON DELETE SET NULL ON UPDATE CASCADE,
+  lesson_id INT NOT NULL REFERENCES lesson ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY (lesson_id)
 );
 
 CREATE TABLE "group_lesson" (
-  instrument_type VARCHAR(500) NOT NULL REFERENCES inventory,
-  lesson_id INT NOT NULL REFERENCES lesson,
+  instrument_type VARCHAR(500) NOT NULL REFERENCES inventory ON DELETE SET NULL ON UPDATE CASCADE,
+  lesson_id INT NOT NULL REFERENCES lesson ON DELETE CASCADE ON UPDATE CASCADE,
   min_students INT,
-  max_students INT NULL
+  max_students INT NULL,
+  PRIMARY KEY (lesson_id)
 );
 
 CREATE TABLE "ensemble" (
-  lesson_id INT NOT NULL REFERENCES lesson,
+  lesson_id INT NOT NULL REFERENCES lesson ON DELETE CASCADE ON UPDATE CASCADE,
   genre VARCHAR(500),
   min_students INT,
-  max_students INT NULL
+  max_students INT NULL,
+  PRIMARY KEY (lesson_id)
+  
 );
 
 
